40af3da9b4279a40082fb99b7858ac652bf2aa7c
Egor <etkachev398@gmail.com>
2024-02-03 14:10:01 +0300

    Added the ability to reset password

--- django_blog/django_blog/authorization/forms.py
+++ django_blog/django_blog/authorization/forms.py
@@ -2,6 +2,7 @@ from django.core.exceptions import ObjectDoesNotExist, ValidationError
 from django.forms import Form, CharField, PasswordInput
 
 from auth_user.models import AuthUser
+from auth_user.validators import validate_password_for_strength
 
 
 class LoginForm(Form):
@@ -10,17 +11,46 @@ class LoginForm(Form):
 
     def clean(self):
         super().clean()
+        user = self.validate_username_or_email()
+        if not user.check_password(self.cleaned_data.get('password')):
+            raise ValidationError({'password': ValidationError('This password is wrong!')})
+
+        return self.cleaned_data
+
+    def validate_username_or_email(self):
         username_or_email = self.cleaned_data.get('username_or_email')
+        user = None
         try:
             if '@' in username_or_email:
                 user = AuthUser.objects.get(email=username_or_email)
             else:
                 user = AuthUser.objects.get(username=username_or_email)
         except ObjectDoesNotExist:
-            raise ValidationError(
-                {'username_or_email': ValidationError("User with such username or email doesn't exist!")})
+            self.add_error('username_or_email', "User with such username or email doesn't exist!")
 
-        if not user.check_password(self.cleaned_data.get('password')):
-            raise ValidationError({'password': ValidationError('This password is wrong!')})
+        return user
+
+
+class ResetPasswordForm(LoginForm):
+    password = None
+
+    class Meta:
+        fields = ['username_or_email']
+
+    def clean(self):
+        self.validate_username_or_email()
+        return self.cleaned_data
+
+
+class SetNewPasswordForm(Form):
+    password = CharField(required=True, max_length=255, widget=PasswordInput,
+                         validators=[validate_password_for_strength], min_length=8)
+    password_confirm = CharField(required=True, max_length=255, widget=PasswordInput, min_length=8)
+
+    def clean(self):
+        super().clean()
+        if self.cleaned_data.get('password') != self.cleaned_data.get('password_confirm') and self.cleaned_data.get(
+                'password') is not None:
+            raise ValidationError({'password_confirm': ValidationError("Passwords must be same!")})
 
         return self.cleaned_data
--- django_blog/django_blog/authorization/urls.py
+++ django_blog/django_blog/authorization/urls.py
@@ -1,9 +1,12 @@
 from django.contrib.auth.decorators import login_required
 from django.urls import path
 
-from authorization.views import LoginView, LogoutView
+from authorization.views import LoginView, LogoutView, ResetPasswordView, EmailVerificationView, SetNewPasswordView
 
 urlpatterns = [
     path('', LoginView.as_view(), name='login'),
+    path('reset-password/', ResetPasswordView.as_view(), name='reset_password'),
+    path('reset-password/<int:pk>/verify-email/', EmailVerificationView.as_view(), name='email_verification'),
+    path('reset-password/<int:pk>/set-new-password/', SetNewPasswordView.as_view(), name='set_new_password'),
     path('logout/', login_required(LogoutView.as_view()), name='logout')
 ]
